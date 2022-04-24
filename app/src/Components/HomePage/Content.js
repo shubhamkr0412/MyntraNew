ebcabe09a2d79743dfe96aab523d34641b967309
Shubham Kumar <shubham35godda@gmail.com>
2022-04-24 08:22:03 +0530

    updated

---
+++ app/src/Components/HomePage/Content.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./Cont.css";
+const Content = () => {
+  return (
+    <div className="cont">
+      <div className="conta">1</div>
+      <div className="conta">2</div>
+      <div className="conta"> 3</div>
+      <div className="conta">4</div>
+    </div>
+  );
+};
+
+export default Content;
--- app/src/Components/HomePage/HomePage.js
+++ app/src/Components/HomePage/HomePage.js
@@ -3,11 +3,13 @@ import Navbar from '../Navbar/Navbar'
 import Slider from '../Slider/Slider'
 import dataSlider from '../Slider/dataSlider'
 import { useState } from 'react';
+import Content from './Content';
 const HomePage = () => {
   const [data, setData] = useState(dataSlider);
   return (
     <div><Navbar/>
     <Slider  data={data} num={7}/>
+   
     </div>
  
   )
